17dc1a54e01c4b771ba83e176fc656b198d77858
AlisonBeltranVillarreal <za210110189@zapopan.tecmm.edu.mx>
2023-02-23 17:50:55 -0600

    first commit

---
+++ Conversor.py
@@ -0,0 +1,24 @@
+#app para convertir pies a metros usando Tkinter.
+#Beltran Alison Judith
+#23 Febrero 2023.
+
+from tkinter import*
+from tkinter import ttk
+
+class Conversor:
+
+    #tipo constructor de la clase
+    def __init__(self, raiz):
+
+        raiz.title("Pies a Metros")
+
+    if __name__=="__main__":
+        print("Este es el archivo Principal. ")
+        print("Nada mas se mostrara esto si es el Principal")
+
+ #   self.pies=StringVar()
+ #   self.metros=StringVar()
+
+ #   mainframe=ttk.Frame(raiz, padding="3 3 12 12")
+ #   mainframe.grind(column=2, row=1, sticky=(W, E))
+
