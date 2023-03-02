c9ca3ec34cc11b761812c9bbd2b506a33fd4b65f
AlisonBeltranVillarreal <za210110189@zapopan.tecmm.edu.mx>
2023-03-02 14:52:01 -0600

    enter

--- Conversor.py
+++ Conversor.py
@@ -12,6 +12,35 @@ class Conversor:
 
         raiz.title("Pies a Metros")
 
+        self.pies=StringVar()
+        self.metros=StringVar()
+
+        mainFrame=ttk.Frame(raiz)
+        mainFrame.grid(column=0, row=0)
+
+        piesEntry=ttk.Entry(mainFrame, textvariable=self.pies)
+        piesEntry.grid(column=1, row=0)
+
+        ttk.Label(mainFrame, text="Pies").grid(column=2, row=0)
+        ttk.Label(mainFrame, text="Son equivalentes").grid(column=0, row=1)
+        ttk.Label(mainFrame, textvariable=self.metros).grid(column=1, row=1)
+        ttk.Label(mainFrame, text="Metros").grid(column=2, row=1)
+
+        ttk.Button(mainFrame, text="Calcular", command=self.calcular).grid(column=2,row=2)
+        
+        #Hcer que funcione con enter
+        raiz.bind("<Return>", self.calcular)
+    
+    def calcular(self, *args):
+        print("Boton Presionado")
+        piesUsuario = float(self.pies.get()) #siempre devuelve la cadena.
+        print("Pies ingresados: ", piesUsuario)
+        piesFlotate = float(piesUsuario) #Conversion de cadena a flotante.
+        metros= piesFlotate*0.3048
+        print("Pies ingresados: ", piesUsuario)
+        print("Metros: ",metros)
+        self.metros.set(metros)
+
     if __name__=="__main__":
         print("Este es el archivo Principal. ")
         print("Nada mas se mostrara esto si es el Principal")
